f43c5982267c14bb3ca4a49943a76c1f031b7865
polinskaya <43246783+polinskaya@users.noreply.github.com>
2018-09-21 04:16:28 +0300

    Update Program.cs

--- oop2/Program.cs
+++ oop2/Program.cs
@@ -97,6 +97,57 @@ namespace oop2
             hip.Insert(5, "_");
             hip.Insert(8, "_");
             Console.WriteLine("\0" + hip);
+ //3
+            //a
+            int[,] mas = new int[3, 3] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            int rows = mas.GetUpperBound(0) + 1;
+            int columns = mas.Length / rows;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{mas[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+            //b
+            string[] weekDays = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+            foreach (String element in weekDays)
+            {
+                Console.WriteLine(element);
+            }
+            int n = weekDays.Length;
+            Console.WriteLine(n);
+            weekDays[0] = weekDays[1];
+            weekDays[1] = weekDays[2];
+            Console.WriteLine("After reversing:" + weekDays[1] + ";" + weekDays[0] + ";" + weekDays[2]);
+            //c
+            int[][] a = { new int[2], new int[3], new int[4] };
+            foreach (int[] x in a)
+            {
+                foreach (int b in x)
+                    Console.Write("\t" + b);
+                Console.WriteLine();
+            }
+            //d
+            //4
+            //a
+            ValueTuple<string, int, char, ulong> student = (name1: "Yulia", age1: 18, surname1: 'P', year1: 2000);
+            //b
+            var me = (one: "Yulia", two: "Polinskaya", three: 18, foure: 2000);
+            string name = me.one;
+            string surname = me.two;
+            int age = me.three;
+            ulong year = 2000;
+            //c
+            Console.WriteLine("\0My name: " + name + "\n My surname: " + surname + "\n my age: " + age + "\0" + year);
+            Console.WriteLine(" I am " + name + "\0" + surname);
+            //d
+            var (one4, two4, three4, foure4) = me;
+            var (name1, age1, surname1, year1) = student;
+            //e
+            Console.WriteLine("\0" + name.Equals(name1));
+            //5
         }
     }
 }
